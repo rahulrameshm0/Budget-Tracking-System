62938ba9af3462a7d5291344b33287a1c4c96eb6
rahulrameshm0 <iamrahulm23@gmail.com>
2025-07-13 06:19:23 +0530

    setuped postgredql

--- budgetTracker/settings.py
+++ budgetTracker/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,10 +75,11 @@ WSGI_APPLICATION = 'budgetTracker.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default':dj_database_url.config(
+       default = 'postgres://postgres:724850@localhost:5432/budget_tracker',
+       conn_max_age = 600,
+       ssl_require = False
+   )
 }
 
 # Password validation
@@ -119,6 +120,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR/'static')
 ]
+
+if os.getenv('RENDER'):
+    SECURE_SSL_REDIRECT = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
