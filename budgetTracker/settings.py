f2adf597d600a25f685469b1533d050b27e7ac4a
rahulrameshm0 <iamrahulm23@gmail.com>
2025-07-19 01:06:24 +0530

    code updated

--- budgetTracker/settings.py
+++ budgetTracker/settings.py
@@ -78,7 +78,6 @@ WSGI_APPLICATION = 'budgetTracker.wsgi.application'
 
 DATABASES = {
    'default':dj_database_url.config(
-       default = os.environ.get('DATABASE_URL'),
        conn_max_age = 600,
        ssl_require = False
    )
